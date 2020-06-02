73321ccd73d95d1ec5a6bf4267ab86bdf9da3806
Bartosz Kalota <bartek.kalota@wp.pl>
2020-06-02 16:20:04 +0200

    Files structure based on routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import injectSheet from 'react-jss';
+import {
+  HashRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
+import injectSheet from 'react-jss';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme, { globalStyle } from './config/theme';
 
+import * as ROUTES from './config/routes';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Logout from './components/Logout';
+import Form from './components/Form';
+import NotFound from './components/NotFound';
+
 const App = () => (
   <ThemeProvider theme={theme}>
-    <h1>App</h1>
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.HOME} component={Home} />
+        <Route path={ROUTES.LOGIN} component={Login} />
+        <Route path={ROUTES.REGISTER} component={Register} />
+        <Route path={ROUTES.LOGOUT} component={Logout} />
+        <Route path={ROUTES.FORM} component={Form} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   </ThemeProvider>
 );
 
