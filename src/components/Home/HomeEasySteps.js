c9c3bf6dce5ca46dd1948a837f0cc93b7f729415
Bartosz Kalota <bartek.kalota@wp.pl>
2020-06-03 13:19:11 +0200

    Home - StepsSection (1) title appearance, (2) moved scroll section id

--- src/components/Home/HomeEasySteps.js
+++ src/components/Home/HomeEasySteps.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Typography } from '@material-ui/core';
+
+import decoration from '../../assets/Decoration.svg';
+
+const useStyles = makeStyles(theme => ({
+  easyStepsSection: {
+    padding: theme.spacing(6, 0, 10, 0)
+  },
+  decoration: {
+    margin: theme.spacing(3, 0, 4, 0)
+  }
+}));
 
 const HomeEasySteps = () => {
+  const classes = useStyles();
   return (
-    <h1>HomeEasySteps</h1>
+    <Grid className={classes.easyStepsSection} id="section2">
+      <Grid item container direction="column" alignItems="center">
+        <Typography variant="h4" component="h3" color="textPrimary">
+          Wystarczą 4 proste kroki
+        </Typography>
+        <img src={decoration} alt="Decoration" className={classes.decoration} />
+      </Grid>
+    </Grid>
   );
 }
  
