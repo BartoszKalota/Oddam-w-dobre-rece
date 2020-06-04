2486d77efcc1f4547812d6e327607d4e11b420b1
Bartosz Kalota <bartek.kalota@wp.pl>
2020-06-04 13:09:15 +0200

    Home - Nav bgr appearance toggle

--- src/components/Home/HomeNav.js
+++ src/components/Home/HomeNav.js
@@ -10,12 +10,20 @@ import {
 import * as ROUTES from '../../config/routes';
 
 const useStyles = makeStyles(theme => ({
-  navSection: {
-    position: 'fixed',
-    top: 0,
-    zIndex: 9,
-    background: 'linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 39%, rgba(255,255,255,0.42) 43%, rgba(255,255,255,1) 49%, rgba(255,255,255,1) 100%)',
-    padding: theme.spacing(2, 0, 4, 0)
+  navSection: isGradient => {
+    const style = {
+      position: 'fixed',
+      top: 0,
+      zIndex: 9,
+      background: isGradient 
+        ? 'linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 39%, rgba(255,255,255,0.42) 43%, rgba(255,255,255,1) 49%, rgba(255,255,255,1) 100%)'
+        : '#FFF',
+      padding: theme.spacing(2, 0, 4, 0)
+    };
+    if (!isGradient) {
+      style.boxShadow = theme.shadows[5];
+    }
+    return style;
   },
   authNavSection: {
     textTransform: 'none',
@@ -60,22 +68,33 @@ const AuthNavigation = () => {
 };
 
 const HomeNav = () => {
-  const classes = useStyles();
   const [isActive, setIsActive] = useState(false);
+  const [isGradient, setIsGradient] = useState(true);
+  const classes = useStyles(isGradient);
 
   useEffect(() => {
+    const windowEl = window;
     const navSection = document.getElementById('section0');
     const headerSection = document.getElementById('section1');
     const threeColsSection = document.getElementById('section1+');
+    // Zarządzanie rodzajem tła sekcji Nav (gradient lub jednolity kolor)
+    let startUniformBgrHeight = headerSection.offsetHeight - navSection.offsetHeight;
+    windowEl.addEventListener('scroll', () => {
+      if (windowEl.pageYOffset > startUniformBgrHeight) {
+        setIsGradient(false);
+      } else {
+        setIsGradient(true);
+      }
+    });
+    // Zarządzenie klasą 'active' dla przycisku 'Start' względem sekcji 'ThreeColumns' (drobna korekta działania react-scroll)
     let startActiveHeight = headerSection.offsetHeight;
     let finishActiveHeight = headerSection.offsetHeight + threeColsSection.offsetHeight - navSection.offsetHeight;
-    // Aktualizacja wysokości sekcji w przypadku zmiany szerokości viewportu
-    const windowEl = window;
+      // Aktualizacja wysokości sekcji w przypadku zmiany szerokości viewportu
     windowEl.addEventListener('resize', () => {
       startActiveHeight = headerSection.offsetHeight;
       finishActiveHeight = headerSection.offsetHeight + threeColsSection.offsetHeight - navSection.offsetHeight;
     });
-    // Zarządzenie klasą 'active' dla przycisku 'Start' względem sekcji 'ThreeColumns' (drobna korekta działania react-scroll)
+      // Określenie zakresu wysokości offsetu, w którym używana jest klasa 'active'
     windowEl.addEventListener('scroll', () => {
       if (
         windowEl.pageYOffset > startActiveHeight &&
