99d9f3fc00c0f7ba84ca4ce1c91d66325ec18ee5
Bartosz Kalota <bartek.kalota@wp.pl>
2020-06-08 15:56:14 +0200

    Home - HelpSection list and pagination appearance

--- src/components/Home/elements/SingleTabListOfOrganisations.js
+++ src/components/Home/elements/SingleTabListOfOrganisations.js
@@ -1,21 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import {
+  Button,
+  Divider,
+  Grid,
+  Typography
+} from '@material-ui/core';
+import clsx from 'clsx';
 
 const useStyles = makeStyles(theme => ({
   descr: {
     maxWidth: 640,
     margin: 'auto',
-    marginBottom: theme.spacing(8),
+    marginBottom: theme.spacing(6),
     color: theme.palette.text.primary,
     lineHeight: 1.2,
     fontSize: '1.4rem'
+  },
+  entityListContainer: {
+    minHeight: 315,
+    marginBottom: theme.spacing(3)
+  },
+  entityContainer: {
+    padding: theme.spacing(3, 0)
+  },
+  entityName: {
+    fontWeight: 300,
+    fontFamily: theme.palette.fontFamilyAlt
+  },
+  divider: {
+    width: '100%',
+    backgroundColor: theme.palette.dividerColor
+  },
+  pageBtn: {
+    minWidth: 40,
+    minHeight: 50,
+    fontSize: '1.2rem',
+    color: theme.palette.text.primary,
+    borderRadius: 0,
+    margin: theme.spacing(0, 1)
+  },
+  activePageBtn: {
+    border: `1px solid ${theme.palette.text.primary}`
   }
 }));
 
 const SingleTabListOfOrganisations = ({ currTab: { descr, entity } }) => {
   const classes = useStyles();
-  const [ counter, setCounter ] = useState(0);
+  const [ counter, setCounter ] = useState(0); // musi być wyjściowo 0 ze względu na entityToShow
 
   const itemsPerPage = 3;
   const entityToShow = entity.slice(counter * itemsPerPage, (counter + 1) * itemsPerPage);
@@ -35,25 +67,46 @@ const SingleTabListOfOrganisations = ({ currTab: { descr, entity } }) => {
       <Typography component="div" align="center" className={classes.descr}>
         {descr}
       </Typography>
-      <ul>
+      <Grid container direction="column" className={classes.entityListContainer}>
         {entityToShow.map(({ name, descr, tags }, i) => (
-          <li key={i}>
-            <p>{name}</p>
-            <p>{descr}</p>
-            <p>{tags.join(', ')}</p>
-          </li>
+          <Fragment key={i}>
+            <Grid item container xs={12} spacing={3} className={classes.entityContainer}>
+              <Grid item xs={7}>
+                <Typography variant="h5" component="p" color="textPrimary" className={classes.entityName}>
+                  {name}
+                </Typography>
+                <Typography variant="body1" component="p" color="textPrimary">
+                  {descr}
+                </Typography>
+              </Grid>
+              <Grid item container xs={5} justify="flex-end" alignItems="center">
+                <Typography variant="body1" component="p" align="right" color="textPrimary">
+                  {tags.join(', ')}
+                </Typography>
+              </Grid>
+            </Grid>
+            {/* Usunięcie ostatniego dividera */}
+            {i !== (entityToShow.length - 1) && <Divider className={classes.divider} />}
+          </Fragment>
         ))}
-      </ul>
-      <div>
-        {numOfPages.map(num => (
-          <button
+      </Grid>
+      <Grid container justify="center">
+        {numOfPages.length > 1 && numOfPages.map((num) => (
+          <Button
             key={num}
             onClick={handleClickPagination}
+            variant="text"
+            size="small"
+            className={
+              (counter + 1) === num
+                ? clsx(classes.pageBtn, classes.activePageBtn)
+                : classes.pageBtn
+            }
           >
             {num}
-          </button>
+          </Button>
         ))}
-      </div>
+      </Grid>
     </Grid>
   );
 }
