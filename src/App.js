14a5dc36cd16ab4e9a123b883b6c539db723686d
Bartosz Kalota <bartek.kalota@wp.pl>
2020-06-26 22:41:01 +0200

    Authorization HOC to Form Page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import injectSheet from 'react-jss';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme, { globalStyle } from './config/theme';
 
+import withAuthorization from './config/session/withAuthorization';
 import * as ROUTES from './config/routes';
 import Home from './components/Home';
 import Form from './components/Form';
@@ -19,7 +20,7 @@ const App = () => (
     <Router>
       <Switch>
         <Route exact path={ROUTES.HOME} component={Home} />
-        <Route path={ROUTES.FORM} component={Form} />
+        <Route path={ROUTES.FORM} component={withAuthorization(Form)} />
         <Route path="*" component={NotFound} />
       </Switch>
     </Router>
